528c3ddf7648140c511c7f413a0b958153dd19bc
Th3c0d3d1 <bittradeguy007@gmail.com>
2024-10-17 14:33:42 -0400

    supply test

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,13 +5,22 @@ pragma solidity ^0.8.0;
 import "./ERC721Enumerable.sol";
 
 contract NFT is ERC721Enumerable {
-        // Inheritted ERC721 constructor params from ERC721Enumerable called as constructor arg
-        // Passes constructor param variables from NFT contract into constructor params from ERC721 contract
-        // ERC721 is parent
-        constructor(
-            string memory _name,
-            string memory _symbol
-            ) ERC721(_name, _symbol) {
-                
+    // Set global variables
+    uint256 public cost;
+    uint256 public maxSupply;
+
+
+    // Inheritted ERC721 constructor params from ERC721Enumerable called as constructor arg
+    // Passes constructor param variables from NFT contract into constructor params from ERC721 contract
+    // ERC721 is parent
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _cost,
+        uint256 _maxSupply
+    // ERC721 doesnt need the cost arg
+    ) ERC721(_name, _symbol) {
+        cost = _cost;
+        maxSupply = _maxSupply;
     }
 }
--- test/NFT.js
+++ test/NFT.js
@@ -11,21 +11,31 @@ describe('NFT', () => {
   // declare global variables
   const NAME = 'First Punk'
   const SYMBOL = 'FPNK'
+  const COST = ether(10)
+  const MAX_SUPPLY = 25
   
   let nft
 
-  beforeEach(async () => {
-    const NFT = await ethers.getContractFactory('NFT')
-    nft = await NFT.deploy('First Punk', 'FPNK')
-  })
-
   describe('Deployment', () => {
+    beforeEach(async () => {
+      const NFT = await ethers.getContractFactory('NFT')
+      nft = await NFT.deploy(NAME, SYMBOL, COST, MAX_SUPPLY)
+    })
+
     it('has correct name', async () => {
       expect(await nft.name()).to.equal(NAME)
     })
 
     it('has correct symbol', async () => {
       expect(await nft.symbol()).to.equal(SYMBOL)
+    })
+
+    it('returns mint cost', async () => {
+      expect(await nft.cost()).to.equal(COST)
+    })
+
+    it('returns max total supply', async () => {
+      expect(await nft.maxSupply()).to.equal(MAX_SUPPLY)
     })
   })
 })
